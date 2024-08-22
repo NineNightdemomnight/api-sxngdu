905b80765d235228b799e6c9649da2f09c4fbe92
NineNightdemomnight <42994065+NineNightdemomnight@users.noreply.github.com>
2024-08-23 04:54:27 +0700

    Create test.js

---
+++ user/test.js
@@ -0,0 +1,4 @@
+// user/test.js
+module.exports = {
+    imageUrl: 'https://cdn.discordapp.com/attachments/1240908938703274015/1276283975450759189/output_image.png?ex=66c8f793&is=66c7a613&hm=a56f9fa2fba524e16f8e8585cbe47505d06f945a9e72af1fb7d24cb2425139f6&'
+};
