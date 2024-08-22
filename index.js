44c1cab7276ac64a4caedfae8201aa5ab87272d6
NineNightdemomnight <42994065+NineNightdemomnight@users.noreply.github.com>
2024-08-23 04:04:38 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
-const express = require('express')
-const app = express()
-const PORT = 4000
+const express = require('express');
+const app = express();
+const PORT = 4000;
+const { apiKey } = require('./code'); // นำเข้ารหัสจาก code.js
 
 app.listen(PORT, () => {
-    console.log(`API Listening on PORT ${PORT}`)
-})
+    console.log(`API Listening on PORT ${PORT}`);
+});
 
 app.get('/', (req, res) => {
-    res.send('This is my API running...')
-})
+    res.send('This is my API running...');
+});
 
 app.get('/about', (req, res) => {
-    res.send('This is my about route')
-})
+    res.send('This is my about route');
+});
 
-module.exports = app
+// Route ใหม่เพื่อตรวจสอบรหัส
+app.get('/check-code', (req, res) => {
+    const { code } = req.query; // รับรหัสจาก query parameter
+
+    if (code === apiKey) {
+        res.send('Welcome to API SXNGDU');
+    } else {
+        res.status(403).send('Unauthorized'); // ใช้รหัสสถานะ 403 สำหรับการเข้าถึงที่ไม่ได้รับอนุญาต
+    }
+});
+
+module.exports = app;
